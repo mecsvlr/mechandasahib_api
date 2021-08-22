74a47e02197017587b30a8baceb9f41cad63f43f
moin2672 <moin2672@gmail.com>
2021-08-22 09:01:42 +0530

    first commit

---
+++ backend/models/organization.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const organizationSchema =  mongoose.Schema({
+    organizationTitle: {type:String, required: true},
+    organizationName: {type:String, required: true},
+    organizationPhoneNo: {type:String,  unique:true, required:true},
+    organizationAddress: {type:String},
+    organizationEmail: {type:String},
+    imgSource: {type:String},
+    oldImgSource: {type:String},
+    isNewImageAdded: {type:Boolean},
+    secretCode: {type:String},
+    lastUpdatedDate: {type:String},
+    creator:{type:mongoose.Schema.Types.ObjectId, ref:"User", required:true}
+});
+
+module.exports = mongoose.model('Organization', organizationSchema);
